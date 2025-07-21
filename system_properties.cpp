db56599914bfa6f662f51b35825c7a498bf7ebc6
topjohnwu <topjohnwu@gmail.com>
2025-07-21 00:20:20 -0700

    Remove logic related to race condition prevention
    resetprop already fundamentally breaks the existing system property
    contract, which is "single writer, multiple reader", so it doesn't
    make sense to spend any effort trying to workaround race conditions.
    
    First of all, since we need to use the same code to handle pre API 30
    devices, we cannot use the dirty backup area mechanism. After removing
    that, we cannot rely on the dirty bit of the serial to function
    correctly, so we skip that too.
    
    Finally, we revert property read back to the old wait implementation
    so that it works on all API levels.

--- system_properties.cpp
+++ system_properties.cpp
@@ -186,9 +186,9 @@ uint32_t SystemProperties::ReadMutablePropertyValue(const prop_info* pi, char* v
     serial = new_serial;
     len = SERIAL_VALUE_LEN(serial);
     if (__predict_false(SERIAL_DIRTY(serial))) {
-      // See the comment in the prop_area constructor.
-      prop_area* pa = contexts_->GetPropAreaForName(pi->name);
-      memcpy(value, pa->dirty_backup_area(), len + 1);
+      __futex_wait(const_cast<atomic_uint_least32_t*>(&pi->serial), serial, nullptr);
+      new_serial = load_const_atomic(&pi->serial, memory_order_relaxed);
+      continue;
     } else {
       memcpy(value, pi->value, len + 1);
     }
@@ -291,25 +291,9 @@ int SystemProperties::Update(prop_info* pi, const char* value, unsigned int len)
   auto* override_pi = const_cast<prop_info*>(have_override ? override_pa->find(pi->name) : nullptr);
 
   uint32_t serial = atomic_load_explicit(&pi->serial, memory_order_relaxed);
-  unsigned int old_len = SERIAL_VALUE_LEN(serial);
-
-  // The contract with readers is that whenever the dirty bit is set, an undamaged copy
-  // of the pre-dirty value is available in the dirty backup area. The fence ensures
-  // that we publish our dirty area update before allowing readers to see a
-  // dirty serial.
-  memcpy(pa->dirty_backup_area(), pi->value, old_len + 1);
-  if (have_override) {
-    memcpy(override_pa->dirty_backup_area(), override_pi->value, old_len + 1);
-  }
   serial |= 1;
-  atomic_store_explicit(&pi->serial, serial, memory_order_release);
-  atomic_thread_fence(memory_order_release);  // Order preceding store w.r.t. memcpy().
   memcpy(pi->value, value, len + 1);
-  // TODO: Eventually replace the above with something like atomic_store_per_byte_memcpy from
-  // wg21.link/p1478 . This is needed for the preceding memcpy and the reader-side copies as well.
-  // In general, memcpy uses near atomic_thread_fence() are suspect.
   if (have_override) {
-    atomic_store_explicit(&override_pi->serial, serial, memory_order_relaxed);
     memcpy(override_pi->value, value, len + 1);
   }
   // Now the primary value property area is up-to-date. Let readers know that they should
